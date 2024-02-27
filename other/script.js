f8f79123d1c8006be31dca482931a8fa1530ecbd
Mateusz <127236338+mateusz-tomaszek@users.noreply.github.com>
2024-02-27 10:52:42 +0100

    Add files via upload

---
+++ other/script.js
@@ -0,0 +1,50 @@
+let countdownTimer;
+let secondsRemaining = 10;
+let isRedirecting = false;
+let timeout;
+
+function startCountdown() {
+    countdownTimer = setInterval(updateCountdown, 1000);
+}
+
+function updateCountdown() {
+    secondsRemaining--;
+    document.getElementById('countdown').textContent = secondsRemaining;
+    if (secondsRemaining === 0) {
+        redirect();
+        document.getElementById('timeoutMessage').style.display = 'none';
+
+    }
+}
+
+function redirect() {
+    if (!isRedirecting) {
+        isRedirecting = true;
+        window.location.href = 'https://mateusztomaszek.com.pl/';
+    }
+}
+
+function resetTimeout() {
+    clearTimeout(timeout);
+    timeout = setTimeout(displayTimeoutMessage, 30000);
+}
+
+function displayTimeoutMessage() {
+    document.getElementById('timeoutMessage').style.display = 'block';
+    document.getElementById('time').style.display = 'block';
+    startCountdown();
+}
+
+document.addEventListener('mousemove', function() {
+    resetTimeout();
+});
+
+document.getElementById('cancelButton').addEventListener('click', function() {
+    clearTimeout(timeout);
+    clearInterval(countdownTimer);
+    isRedirecting = false;
+    document.getElementById('timeoutMessage').style.display = 'none';
+    document.getElementById('time').style.display = 'none';
+});
+
+resetTimeout();
